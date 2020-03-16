6f2fae3abbcd363cf3321e939485661db628973a
CyberThink777 <alexanderm61@gmail.com>
2020-03-16 17:39:26 +0800

    Finished migrate to rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use serde::{Deserialize};
+use serde::Deserialize;
 use std::fs;
 
 #[derive(Deserialize)]
@@ -13,12 +13,25 @@ struct Server {
 pub fn run(filename: &str, group: &str) {
     let contents = fs::read_to_string(filename).expect("Something went wrong when reading files");
     let servers = parse(contents);
+    let group_base_64 = base64::encode_config(group, base64::STANDARD_NO_PAD);
+    for server in servers {
+        encode_and_print(server, &group_base_64)
+    }
 }
 
 fn parse(data: String) -> Vec<Server> {
     serde_json::from_str(&data).expect("Error parsing JSON!")
 }
 
-/* fn encode_and_print(servers: Server, group: &str) {
-    let ssrlink;
-} */
+fn encode_and_print(server: Server, group: &String) {
+    let pass = base64::encode_config(server.password, base64::STANDARD_NO_PAD);
+    let remarks = base64::encode_config(server.remarks, base64::STANDARD_NO_PAD);
+    let ssrlink = format!(
+        "{}:{}:origin:{}:plain:{}/?remarks={}&group={}",
+        server.server, server.server_port, server.method, pass, remarks, group
+    );
+    println!(
+        "ssr://{}",
+        base64::encode_config(ssrlink, base64::STANDARD_NO_PAD)
+    );
+}
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ use std::env;
 use std::process;
 
 fn main() {
-    let args: Vec<String> = env::args().collect(); 
+    let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         println!("Usage: {} [FILENAME] <GroupName>", &args[0]);
         process::exit(1);
     }
     match args.get(2) {
         Some(group) => ssrencoder::run(&args[1], group),
-        None => ssrencoder::run(&args[1], "Untitled")
+        None => ssrencoder::run(&args[1], "Untitled"),
     }
 }
